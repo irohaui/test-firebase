89ee5d8157e342f33b2886169708b80aa8535e37
ui mizuhara <ui.mizuhara@gmail.com>
2018-05-21 10:42:22 +0900

    toppage and firebase

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module TestFirebase
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.generator do |g|
+    config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
         view_specs: false,
